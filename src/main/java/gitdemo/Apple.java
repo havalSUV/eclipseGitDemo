7f93fab26af7f98d6830d5bf42ff69755fbdefbc
tom_glb <goodMorning_glb@atguigu.com>
2019-08-12 20:24:32 +0800

    aaaaa

--- src/main/java/gitdemo/Apple.java
+++ src/main/java/gitdemo/Apple.java
@@ -2,19 +2,15 @@ package gitdemo;
 
 public class Apple {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
+	public static void main(String[] args) { 
 		System.out.println("厘頁2.。。。。。。。。。。。。。。"); 
-		System.out.println("厘頁hot_fix...!!!");
-=======
+		System.out.println("厘頁hot_fix...!!!"); 
 		System.out.println("厘頁2.。。。。。。。。。。。。。。");
 		System.out.println("厘頁2.。。。。。。。。。。。。。。");
 		System.out.println("厘頁2.。。。。。。。。。。。。。。");
 		System.out.println("厘頁2.。。。。。。。。。。。。。。");
 		
-		System.out.println("厘頁1.。。。。。。。。。。。。。。");
->>>>>>> branch 'master' of https://github.com/havalSUV/eclipseGitDemo.git
+		System.out.println("厘頁1.。。。。。。。。。。。。。。"); 
 	}
 
 }
